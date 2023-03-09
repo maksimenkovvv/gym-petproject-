c7882eeb74333e4c9de42ccdbb96015879471f94
Anton Maksimenkov <104681708+maksimenkovvv@users.noreply.github.com>
2023-03-09 14:59:12 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let menuBtnOpen = document.querySelector(".menu__button");
+let menuBtnClose = document.querySelector(".modal__menu__close-btn");
+let menu = document.querySelector(".modal__menu__container");
+
+menuBtnOpen.addEventListener("click", function () {
+  menu.classList.toggle("active");
+});
+
+menuBtnClose.addEventListener("click", function () {
+  menu.classList.toggle("active");
+});
